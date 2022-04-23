2da466f08d74162b7a30965ebb10863b523b5dfe
ufdds025su <ufdds025su@yahoo.com.tw>
2022-04-24 07:02:57 +0800

    Create diy-part2.sh

---
+++ diy-part2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 版本号里显示一个自己的名字
+# sed -i "s/OpenWrt/Randy build $(TZ=UTC-8 date "+%y.%m.%d") @/g" package/lean/default-settings/files/zzz-default-settings
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改 argon 为默认主题,不再集成luci-theme-bootstrap主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 获取luci-app-openclash 编译po2lmo
+# git clone -b master https://github.com/vernesong/OpenClash package/diy-packages/openclash
+# pushd package/diy-packages/openclash/luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
