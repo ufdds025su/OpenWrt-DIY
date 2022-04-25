1e7eaa2e8da8997f08558f58421cff78e7fe6de9
ufdds025su <ufdds025su@yahoo.com.tw>
2022-04-25 09:53:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.120.1/g' package/base-files/files/bin/config_generate
 
 # 版本号里显示一个自己的名字
 # sed -i "s/OpenWrt/Randy build $(TZ=UTC-8 date "+%y.%m.%d") @/g" package/lean/default-settings/files/zzz-default-settings
