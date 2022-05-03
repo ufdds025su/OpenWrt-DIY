fa504b50f6edb2329fade8e06c4eeb3e4e2d4e8e
ufdds025su <ufdds025su@yahoo.com.tw>
2022-05-03 18:14:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,13 +25,3 @@ git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/l
 #添加额外软件包
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-
-# ssr plus
-# svn co https://github.com/fw876/helloworld/trunk/tcping package/tcping
-# svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
-# svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
-# svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
-# svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
-# svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
-# svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
-# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
