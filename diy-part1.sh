95a1c5266a6e7a1a17ebe6244b62387e53924f78
ufdds025su <ufdds025su@yahoo.com.tw>
2022-05-11 21:10:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,16 +11,16 @@
 #
 
 # Add a feed source
-#sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
 # Add luci-theme-argon
 # rm -rf lede/package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-theme-argon.git
-rm -rf package/lean/luci-theme-argon/
+#rm -rf package/lean/luci-theme-argon/
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #添加额外软件包
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
