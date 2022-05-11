04bde5263d86eb2eea4a722ca8f81986fc83c24a
ufdds025su <ufdds025su@yahoo.com.tw>
2022-05-11 14:15:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Add a feed source
-sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
